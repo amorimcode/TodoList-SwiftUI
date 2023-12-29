6028cb8a6bbaaf886c6fcf828e834435418e16b7
amorim <bruno.amorim032@gmail.com>
2023-12-29 16:51:04 -0300

    add check logic

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 struct ListView: View {
-    @State var items: [ItemModel] = [
-        ItemModel(title: "First title", isCompleted: false),
-        ItemModel(title: "Second title", isCompleted: true),
-        ItemModel(title: "Third title", isCompleted: false),
-    ]
+    
+    @EnvironmentObject var listViewModel: ListViewModel
     
     
     var body: some View {
         List {
-            ForEach(items) { item in
+            ForEach(listViewModel.items) { item in
                 ListRowView(item: item)
+                    .onTapGesture {
+                        withAnimation(.linear) {
+                            listViewModel.updateItem(item: item)
+                        }
+                    }
             }
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
         .navigationTitle("Todo List 📋")
@@ -32,6 +36,7 @@ struct ListView: View {
 #Preview {
     NavigationView {
         ListView()
+            .environmentObject(ListViewModel())
     }
 }
 
