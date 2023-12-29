526dc03e6dc3fed792cc9ec5d0554c01deb9bd00
Bruno Amorim <bruno.amorim@mblabs.com.br>
2023-12-29 13:34:29 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Bruno Amorim on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
